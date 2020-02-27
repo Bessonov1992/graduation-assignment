49bcb611fde9ec79eb50363d2eceafa92f5da48d
Дмитрий <dnitriy@MacBook-Pro-Dmitrij.local>
2020-02-27 21:33:34 +0200

    Ver 2.0

--- Menu.py
+++ Menu.py
@@ -1,33 +1,53 @@
 from tkinter import *
-class main:
-  def __init__(self, master):
-    self.master = master
-    self.master.title('Program')
-    self.master.geometry('150x150+300+225')
-    self.button = Button(self.master,
-                         text = 'Создать запись',
-                         command = self.openDialog)
-    self.button.pack(side = TOP)
-    self.button = Button(self.master,
-                         text='Найти запись',
-                         command=self.openDialog)
-    self.button.pack(side=TOP)
-    self.button = Button(self.master,
-                         text='Редактировать запись',
-                         command=self.openDialog)
-    self.button.pack(side=TOP)
-    self.button = Button(self.master,
-                         text='Удалить запись',
-                         command=self.openDialog)
-    self.button.pack(side=TOP)
-    self.button = Button(self.master,
-                         text='Выйти из программы',
-                         command=self.openDialog)
-    self.button.pack(side=TOP)
-    self.master.mainloop()
 
-  def openDialog(self):
-    child(self.master)
+
+
+def child(master):
+    pass
+
+class main:
+    def __init__(self, master):
+        self.master = master
+        self.master.title('Program')
+        self.master.geometry('150x150+300+225')
+
+        self.button = Button(self.master,
+                             text='Создать запись',
+                             command=self.openDialog)
+        def crt(event):
+            import Create
+        self.button.pack(side=TOP)
+        self.button.bind('<Button-1>', crt)
+
+
+        self.button = Button(self.master,
+                             text='Найти запись',
+                             command=self.openDialog)
+        self.button.pack(side=TOP)
+
+
+        self.button = Button(self.master,
+                             text='Редактировать запись',
+                             command=self.openDialog)
+        self.button.pack(side=TOP)
+
+        self.button = Button(self.master,
+                             text='Удалить запись',
+                             command=self.openDialog)
+        self.button.pack(side=TOP)
+
+        self.button = Button(self.master,
+                             text='Выйти из программы',
+                             command=self.openDialog)
+        self.button.pack(side=TOP)
+
+        self.master.mainloop()
+
+    def openDialog(self):
+        child(self.master)
+
+
+
 root = Tk()
 main(root)
 
